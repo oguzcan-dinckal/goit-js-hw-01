89eab7e0d0f6b793aee34033baf854d970f8374a
oguzcan-dinckal <oguzcandinckal00@gmail.com>
2024-08-22 13:36:20 +0300

    Delete goit-js-hw-01-main directory

--- goit-js-hw-01-main/README.md
+++
@@ -1,1 +0,0 @@
-This project created for GoIT first JavaScript module homework.
--- goit-js-hw-01-main/js/task-1.js
+++
@@ -1,6 +0,0 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
-
-console.log(makeTransaction(5, 3000));
